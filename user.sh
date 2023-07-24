bc41d35686d5fab931d79d68d22a27003114201b
Chinna <chinna.aduri@gmail.com>
2023-07-24 02:15:05 -0700

    user function

--- user.sh
+++ user.sh
@@ -1,16 +1,4 @@
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-cp user.service /etc/systemd/system/user.service
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-systemctl enable user
-systemctl start user
-yum install mongodb-org-shell -y
-mongo --host 172.31.95.239 </app/schema/user.js
-systemctl restart user
+component=user
+source=common.sh
+
+nodejs
