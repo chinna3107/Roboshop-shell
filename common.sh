f3450f45ed213fa67bb72c7a26cab9ef5d38c5b7
Chinna <chinna.aduri@gmail.com>
2023-07-25 16:45:58 -0700

    catalogue function

--- common.sh
+++ common.sh
@@ -1,18 +1,4 @@
-func_nodejs(){
-
-log=/tmp/roboshop.log
-echo -e "\e[36m<<<< creating ${component} >>>>>\e[0m" | tee -a ${log}
-cp ${component}.service  /etc/systemd/system/${component}.service &>>${log}
-
-echo -e "\e[36m<<<< creating mongo repo >>>>\e[0m" | tee -a /tmp/roboshop.log
-cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-
-echo -e "\e[36m<<<< installing nodejs repo >>>>\e[0m" | tee -a ${log}
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
-
-echo -e "\e[36m<<<< Installing nodejs >>>>\e[0m" | tee -a ${log}
-yum install nodejs -y &>>/tmp/roboshop.log
-
+Func_apppreq(){
 echo -e "\e[36m<<<< creating roboshop user account >>>>\e[0m" | tee -a ${log}
 useradd roboshop &>>${log}
 echo -e "\e[36m<<<< Remove App folder  >>>>\e[0m" | tee -a ${log}
@@ -28,6 +14,31 @@ echo -e "\e[36m<<<< Extracting application content  >>>>\e[0m" | tee -a ${log}
 cd /app &>>${log}
 unzip /tmp/${component}.zip &>>${log}
 cd /app &>>${log}
+}
+
+func_systemd() {
+echo -e "\e[36m<<<< Start ${component} service  >>>>\e[0m" | tee -a ${log}
+systemctl daemon-reload &>>${log}
+systemctl enable ${component} &>>${log}
+systemctl restart ${component} &>>${log}
+}
+
+func_nodejs(){
+
+log=/tmp/roboshop.log
+echo -e "\e[36m<<<< creating ${component} >>>>>\e[0m" | tee -a ${log}
+cp ${component}.service  /etc/systemd/system/${component}.service &>>${log}
+
+echo -e "\e[36m<<<< creating mongo repo >>>>\e[0m" | tee -a /tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+
+echo -e "\e[36m<<<< installing nodejs repo >>>>\e[0m" | tee -a ${log}
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+
+echo -e "\e[36m<<<< Installing nodejs >>>>\e[0m" | tee -a ${log}
+yum install nodejs -y &>>/tmp/roboshop.log
+
+func_apppreq
 
 echo -e "\e[36m<<<< Download NodeJs Dependencies  >>>>\e[0m" | tee -a ${log}
 npm install &>>${log}
@@ -38,10 +49,8 @@ yum install mongodb-org-shell -y &>>${log}
 echo -e "\e[36m<<<<   Load Application content >>>>\e[0m" | tee -a ${log}
 mongo --host mongodb.devops-tools.online </app/schema/${component}.js &>>${log}
 
-echo -e "\e[36m<<<< Start ${component} service  >>>>\e[0m" | tee -a ${log}
-systemctl daemon-reload &>>${log}
-systemctl enable ${component} &>>${log}
-systemctl restart ${component} &>>${log}
+func_systemd
+
 }
 func_java{
 }
