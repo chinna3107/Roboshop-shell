2caa25c8b71ea8463455ab88c57a5bf334cdb9f9
Chinna <chinna.aduri@gmail.com>
2023-07-26 08:53:30 -0700

    redis function

--- redis.sh
+++ redis.sh
@@ -1,6 +1,8 @@
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-yum module enable redis:remi-6.2 -y
-yum install redis -y
-systemctl enable redis
-systemctl restart redis
+log=/tmp/roboshop.log
+echo -e "\e[36m<<<< creating ${component} >>>>>\e[0m" | tee -a ${log}
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
+yum module enable redis:remi-6.2 -y &>>${log}
+yum install redis -y &>>${log}
+systemctl enable redis &>>${log}
+systemctl restart redis &>>${log}
 
