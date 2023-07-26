82ac94e4215f3c0f813ccc06b9595364261800c4
Chinna <chinna.aduri@gmail.com>
2023-07-26 08:54:26 -0700

    redis function

--- redis.sh
+++ redis.sh
@@ -1,5 +1,5 @@
 log=/tmp/roboshop.log
-echo -e "\e[36m<<<< creating ${component} >>>>>\e[0m" | tee -a ${log}
+echo -e "\e[36m<<<< creating Radis  >>>>>\e[0m" | tee -a ${log}
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
 yum module enable redis:remi-6.2 -y &>>${log}
 yum install redis -y &>>${log}
